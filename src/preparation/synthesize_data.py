c6f41b45eac10920e3a8f42330905afd7aeca151
stash <ratatouille.apple@gmail.com>
2021-03-17 15:40:14 +0900

    add synthesize data

---
+++ src/preparation/synthesize_data.py
@@ -0,0 +1,23 @@
+from os.path import join
+import numpy as np
+import torch
+import pandas as pd
+
+
+def synthesize_data(f, f_name, project_root, input_dim, N=10000):
+    input = torch.rand(N, input_dim)
+    output = f(input)
+    print(min(output).item(), max(output).item())
+    y = [np.random.choice([1, 0], p=[p.item(), 1 - p.item()]) for p in output]
+    df = pd.DataFrame(
+        torch.cat((input, output), dim=1).detach().numpy(),
+        columns=["x1", "x2", "target_p"],
+    )
+    df["target_y"] = y
+    # write a pandas dataframe to gzipped CSV file
+
+    path = join(project_root, f"data/raw/{f_name}")
+    print("saving to :", path)
+    df.to_csv(path, index=False, compression="gzip")
+    print("saved")
+    return df

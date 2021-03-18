342366fb625d7a31f3eac35b05194bd2ef73ba7e
stash <ratatouille.apple@gmail.com>
2021-03-18 14:38:28 +0900

    modify model

--- model/__init__.py
+++ model/__init__.py
@@ -1,4 +1,5 @@
 from model.hidden1 import LitHidden1
 from model.parent_model import LitParentModel
+from model.normalized_cos import normalized_cos
 
-# __all__ = ["LitHidden1", "LitParentModel"]
+__all__ = ["LitHidden1", "LitParentModel", "normalized_cos"]

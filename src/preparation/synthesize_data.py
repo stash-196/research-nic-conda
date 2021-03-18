3803fd42be4ae2b9b830fc8e727d67c561d58637
stash <ratatouille.apple@gmail.com>
2021-03-18 14:39:30 +0900

    delete print

--- src/preparation/synthesize_data.py
+++ src/preparation/synthesize_data.py
@@ -7,7 +7,6 @@ import pandas as pd
 def synthesize_data(f, f_name, project_root, input_dim, N=10000):
     input = torch.rand(N, input_dim)
     output = f(input)
-    print("output:\n", output.shape, "input:\n", input.shape)
     y = [np.random.choice([1, 0], p=[p.item(), 1 - p.item()]) for p in output]
     df = pd.DataFrame(
         torch.cat((input, output), dim=1).detach().numpy(),
